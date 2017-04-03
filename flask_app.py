165ba9ce8cbe2fc2d1ab009f796cabf9a186f1b7
Chris Aring <christopher.aring@students.olin.edu>
2017-04-03 13:48:59 -0400

    Toolbox Donw!

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,18 @@
-"""
-Put your Flask app code here.
-"""
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    name = (request.form['name'])
+    age = request.form['age']
+    favorite = (request.form['favorite'])
+    if name and age and favorite:
+        return render_template('user.html', user = name, age = age)
+    else:
+        return render_template('error.html')
